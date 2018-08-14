61d74e287d89f634232d16295ae5593ac3057133
Ray Meredith <ray.meredith@gmail.com>
2018-08-14 18:16:21 -0500

    basic app set-up

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <a href="/" class="active item">
+            Image Storage
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader'
+};
+</script>
+
+<style>
+</style>
