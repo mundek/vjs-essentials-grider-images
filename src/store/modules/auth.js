938f9421aab97a6c9004d219f579ec391785551e
Ray Meredith <ray.meredith@gmail.com>
2018-08-15 16:28:46 -0500

    code at end of section 5

---
+++ src/store/modules/auth.js
@@ -0,0 +1,34 @@
+import api from '../../api/imgur';
+
+const state = {
+    token: null
+};
+
+const getters = {
+    isLoggedIn: (state) => {
+        // double exclamations operator? forces evaluation of expressions as Boolean
+        return !!state.token;
+    }
+};
+
+const actions = {
+    login: () => {
+        api.login();
+    },
+    logout: ({ commit })  => {
+        commit('setToken', null)
+    }
+};
+
+const mutations = {
+    setToken: (state, token) => {
+        state.token = token;
+    }
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
